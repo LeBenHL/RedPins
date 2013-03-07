dd3ce7f23e681b4983f7205b9d8d3a91f2ce15c8
Benjamin Le <kantas92@berkeley.edu>
2013-03-07 04:05:36 -0800

    Added in Navigation Fragment which will be the homepage of the app after logging in. This is the page with all the buttons on the Front and the Search Bar on Top

---
+++ java/android/app/RedPins/src/com/example/redpins/NavigationFragment.java
@@ -0,0 +1,64 @@
+package com.example.redpins;
+
+import java.util.Arrays;
+
+import com.facebook.widget.LoginButton;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NavigationFragment extends Fragment {
+	
+	private View nearbyButton;
+	private View recommendationsButton;
+	private View addEventButton;
+	private View bookmarksButton;
+	private View profileButton;
+	private View historyButton;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, 
+	        ViewGroup container, 
+	        Bundle savedInstanceState) {
+	    View view = inflater.inflate(R.layout.navigation_fragment, container, false);
+	    nearbyButton = view.findViewById(R.id.Nearby);
+	    recommendationsButton = view.findViewById(R.id.Recommendations);
+	    addEventButton = view.findViewById(R.id.AddEvent);
+	    bookmarksButton = view.findViewById(R.id.Bookmarks);
+	    profileButton = view.findViewById(R.id.Profile);
+	    historyButton = view.findViewById(R.id.History);
+	    
+	    return view;
+	}
+	
+	public void nearbyOnClick(View view) {
+		Log.v("buttonClick", "NEARBY");
+	}
+	
+	public void recommendationsOnClick(View view) {
+		Log.v("buttonClick", "RECOMMENDATIONS");
+	}
+	
+	public void addEventOnClick(View view) {
+		Log.v("buttonClick", "ADDEVENT");
+	}
+	
+	public void bookmarksOnClick(View view) {
+		Log.v("buttonClick", "BOOKMARKS");
+	}
+	
+	public void profileOnClick(View view) {
+		Log.v("buttonClick", "PROFILE");
+	}
+	
+	public void historyOnClick(View view) {
+		Log.v("buttonClick", "HISTORY");
+	}
+
+
+}
