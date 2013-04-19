3b3cfc6789ddd1a3970ef307a36d6179a25f2241
Soo Hyoung (Eric) Cheong <eric9194@gmail.com>
2013-04-18 17:10:50 -0700

    set up frame for requestServer, but not implemented yet.

--- java/android/app/RedPinsTest/src/com/example/redpins/test/MockUtility.java
+++ java/android/app/RedPinsTest/src/com/example/redpins/test/MockUtility.java
@@ -6,9 +6,36 @@ import com.example.redpins.*;
 
 public class MockUtility extends Utility {
 
-//	//@Override
-//	private JSONObject requestServer(String url, JSONObject jsonInput) {
-//		// switch statement between request URLs, return customized JSONs 
-//		return null;
-//	}
+	public static JSONObject requestServer(String url, JSONObject jsonInput) {
+		// switch statement between request URLs, return customized JSONs 
+		JSONObject response = new JSONObject();
+		if(url.equals(MainActivity.serverURL+"/users/postComment.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/BookmarkEvent.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/uploadPhoto")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/events/getComments.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/events/get.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/getBookmarks.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/alreadyLikedEvent.json")){
+		
+		}else if(url.equals(MainActivity.serverURL+"/events/search.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/cancelEvent.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/removeComment.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/deleteEvent.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/removeBookmark.json")){
+			
+		}else if(url.equals(MainActivity.serverURL+"/users/likeEvent.json")){
+			
+		}
+		return response;
+	}
 }
